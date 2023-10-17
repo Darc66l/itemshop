8068b8dd5008a72aa4b674260c3c4e68ca515b45
Darkc66l <satybaldiew.ruslan@gmail.com>
2023-10-17 18:00:42 +0600

    lesson_04 dz files

--- stepshop/mainapp/views.py
+++ stepshop/mainapp/views.py
@@ -1,37 +1,44 @@
 from django.shortcuts import render
 
-def index(request):
-    title="Home"
+
+def get_cont(title):
     links_menu = [{'link':'index','name':'Главная'},
                   {'link':'about','name':'О нас'},
                   {'link':'products:index','name':'Продукты'},
                   {'link':'contact','name':'Контакты'},
-    
-                  ]
+    ]
+
     context = {'title': title,
                'links_menu':links_menu,
     }
+    return context
+
+
+
+def index(request):
+    title="Home"
+    context = get_cont(title)
 
     return render(request, 'index.html',context)
 def about(request):
     title="About US"
     
-    context = {'title': title}
+    context = get_cont(title)
 
     return render(request, 'about.html',context)
 def products(request):
     title="Каталог продуктов"
-    context = {'title': title}
+    context = get_cont(title)
 
     return render(request, 'products.html',context)
 def contact(request):
     title="Contacts"
-    context = {'title': title}
+    context = get_cont(title)
 
     return render(request, 'contact.html',context)
 def product(request):
     title="Product"
-    context = {'title': title}
+    context = get_cont(title)
 
     return render(request, 'single-product.html',context)
 
